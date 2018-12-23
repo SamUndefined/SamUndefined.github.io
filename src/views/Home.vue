9fe4a6b248dc362a0a242c8fedc6f40258889342
SamUndefined <sam.undefined.dev@gmail.com>
2018-12-22 18:44:01 -0600

    Ignore dist on main branch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="content">
         <h1>Hello World!</h1>
-        <p>This website is a work-in-progress. For now, things are mostly empty.</p>
+        <p>This website is a work-in-progress. For now, things are empty.</p>
     </div>
   </div>
 </template>
