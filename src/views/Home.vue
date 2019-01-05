cc0af86266d502e7c0ba8c9ae1d0796dbe9420e1
SamUndefined <sam.undefined.dev@gmail.com>
2019-01-05 13:56:42 -0600

    Add links to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,23 @@
   <div class="home">
     <div class="content">
         <h1>Hello World!</h1>
-        <p>This website is a work-in-progress. For now, things are empty.</p>
+        <p>This website is a work-in-progress. In the meantime, have some links:</p>
+        <h3 class="link">
+            <b-icon icon="github-circle" size="is-big" type="is-dark"></b-icon>&nbsp;
+            GitHub: <a href="https://github.com/samundefined">https://github.com/samundefined</a>
+        </h3>
+        <h3 class="link">
+            <b-icon icon="twitch" size="is-big" type="is-primary"></b-icon>&nbsp;
+            Twitch: <a href="https://twitch.tv/samundefined">https://twitch.tv/samundefined</a>
+        </h3>
+        <h3 class="link">
+            <b-icon icon="twitter" size="is-big" type="is-info"></b-icon>&nbsp;
+            Twitter: <a href="https://twitter.com/samundefineddev">https://twitter.com/samundefineddev</a>
+        </h3>
+        <h3 class="link">
+            <b-icon icon="email" size="is-big" type="is-success"></b-icon>&nbsp;
+            Email: <a href="mailto:sam.undefined.dev@gmail.com">sam.undefined.dev@gmail.com</a>
+        </h3>
     </div>
   </div>
 </template>
@@ -12,7 +28,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.home {
+    margin-left: 5rem;
+}
+.link {
+    font-weight: 100;
+}
 </style>
 
